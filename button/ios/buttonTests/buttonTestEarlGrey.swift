f2286ee6a9c40260a6f52693a004999712456df6
bootstraponline <code@bootstraponline.com>
2017-01-03 15:44:41 -0500

    Set accessible to true

--- button/ios/buttonTests/buttonTestEarlGrey.swift
+++ button/ios/buttonTests/buttonTestEarlGrey.swift
@@ -8,14 +8,11 @@ class EarlGreyExampleSwiftTests: XCTestCase {
   }
 
   func testImage() {
-    /*
-     Why is this failing?
-     
-     [buttonTests.EarlGreyExampleSwiftTests testImage] : Assertion 'assertWithMatcher: matcherForSufficientlyVisible(>=0.750000)' was not performed because no UI element matching ((respondsToSelector(isAccessibilityElement) && isAccessibilityElement) && accessibilityLabel("automation_image_label")) was found.
-
-
-     <RCTImageView:0x7fd30a9020d0; AX=N; AX.id='automation_image_id'; AX.label='automation_image_label'; AX.frame={{133, 319}, {148, 136}}; AX.activationPoint={207, 387}; AX.traits='UIAccessibilityTraitImage'; AX.focused='N'; frame={{133, 319}, {148, 136}}; alpha=1>
-    */
+    // grey_accessibilityLabel does not match on AX.N to be
+    // consistent with accessibility inspector / voice over.
+    //
+    // react-native must set accessibility: true on elements to match on them by label
+    // https://facebook.github.io/react-native/docs/accessibility.html
     EarlGrey.select(elementWithMatcher: grey_accessibilityLabel("automation_image_label")).assert(grey_sufficientlyVisible());
 
     EarlGrey.select(elementWithMatcher: grey_accessibilityID("automation_image_id")).assert(grey_sufficientlyVisible());
--- button/test_label.js
+++ button/test_label.js
@@ -3,6 +3,7 @@ function testLabel(description) {
   // accessibilityLabel | accessibilityLabel | content description
   // testID             | accessibilityID    | view tag
   return {
+                accessible: true,
                     testID: description + "_id",
         accessibilityLabel: description + "_label"
   }
