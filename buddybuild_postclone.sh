b6ea541b11a9da560cb10ba54715f270d9d77cb4
bootstraponline <code@bootstraponline.com>
2017-01-26 15:55:54 -0500

    Update buddybuild_postclone.sh

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# list rubies
+# list rubies (ruby-2.2.5, ruby-2.3.1)
 chruby
 
 # select ruby
 chruby ruby-2.3.1
 
 # list all files
-find / .
+# find / . > files.txt
 
 echo "exiting!"
 
