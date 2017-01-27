dbf842edd8c1ac4ae598572c26c855c42c7eb2a2
bootstraponline <code@bootstraponline.com>
2017-01-27 11:26:22 -0500

    Update buddybuild_postclone.sh

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -9,11 +9,23 @@ chruby ruby-2.3.1
 # list all files
 # find / . > files.txt
 
+# install gcloud sdk
 curl -L https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-141.0.0-darwin-x86_64.tar.gz | tar xz
 
-# tar -zxf google-cloud-sdk-*.tar.gz
+# add 'gcloud' to path
+export PATH="`pwd`/google-cloud-sdk/bin:$PATH"
 
+# update
+gcloud --quiet components update
+
+# authenticate
+# echo $GCLOUD_KEY | base64 --decode > "gcloudkey.json"
+# gcloud config set project "$GCLOUD_PROJECT"
+# gcloud auth activate-service-account --key-file "gcloudkey.json" "$GCLOUD_USER"
+
+# list folders
 ls -la
 
+# exit early
 echo "finished"
 exit 1
