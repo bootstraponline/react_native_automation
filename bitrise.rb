db8c892833e449fb9ee36bb4383daaf22a5c4f61
bootstraponline <code@bootstraponline.com>
2017-03-05 13:16:22 -0500

    Update copy logic

--- bitrise.rb
+++ bitrise.rb
@@ -8,7 +8,7 @@ def join(*args)
   File.expand_path(File.join(*args))
 end
 
-def app_path bundle_id
+def app_path(bundle_id)
   path = `xcrun simctl get_app_container booted #{bundle_id}`.strip
   raise 'get_app_container command failed' unless $CHILD_STATUS.success?
   path = join(path, '../' * 4, 'Data/Application')
@@ -16,7 +16,7 @@ def app_path bundle_id
   path
 end
 
-def metadata_id plist
+def metadata_id(plist)
   result = `/usr/libexec/PlistBuddy -c 'Print MCMMetadataIdentifier' "#{plist}"`.strip
   raise 'PlistBuddy command failed' unless $CHILD_STATUS.success?
   result
@@ -33,19 +33,13 @@ Dir.glob(search_glob) do |plist_path|
     documents_dir = join(File.dirname(plist_path), 'Documents')
     break
   end
-
 end
 
 raise 'failed to find documents dir for app' unless documents_dir
-
 puts "EarlGrey documents found: #{documents_dir}"
 
-xcode_test_results = ENV['BITRISE_XCODE_RAW_TEST_RESULT_TEXT_PATH']
-
-if xcode_test_results
-  FileUtils.cp xcode_test_results, $BITRISE_DEPLOY_DIR
-end
-
-FileUtils.cp documents_dir, $BITRISE_DEPLOY_DIR
+# any files in $BITRISE_DEPLOY_DIR wil be attached to the build
+# raw-xcodebuild-output.log is saved there by default.
+FileUtils.cp_r documents_dir, $BITRISE_DEPLOY_DIR
 
 puts 'Finished saving test artifacts'
